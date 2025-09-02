2bec00b76a7fd041c84d6ebd2cf56b416d22c0e3
Jessica Wayde <jess@electriccitizen.com>
2025-09-02 10:37:30 -0700

    Upgrade to Drupal 11.3

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -861,3 +861,9 @@ $settings['config_sync_directory'] = 'config/default/sync';
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
