f22333ba71679ba2d67ca99befb29ed35b4bc00c
Jessica Wayde <jess@electriccitizen.com>
2026-06-22 13:17:09 -0700

    add some highlights and fix the gulpfile

--- themes/waxo_gray/gulpfile.js
+++ themes/waxo_gray/gulpfile.js
@@ -1,4 +1,4 @@
-const { watch, parallel, series, dest, src } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const $ = require('gulp-load-plugins')();
 const autoprefixer = require('autoprefixer');
@@ -10,25 +10,26 @@ const sassPaths = [
 
 function taskSass(cb) {
   console.log('Rendering SCSS...');
-  const returned = src('scss/app.scss')
+  const returned = gulp.src('scss/app.scss')
     .pipe(sass({
       includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
+      outputStyle: 'compressed'
     })
-      .on('error', sass.logError))
+      .on('error', sass.logError)
+    )
     .pipe($.postcss([
       autoprefixer(),
     ]))
-    .pipe(dest('./css'));
+    .pipe(gulp.dest('./css'));
   console.log('Render complete!');
   return returned;
 }
 
 function taskWatch(cb) {
-  watch('scss/*.scss', taskSass);
+  gulp.watch('scss/*.scss', taskSass);
   cb();
 }
 
-exports.default = series(taskSass);
-exports.build = series(taskSass);
-exports.dev = parallel(taskWatch, series(taskSass));
+exports.default = gulp.series(taskSass);
+exports.build = gulp.series(taskSass);
+exports.dev = gulp.parallel(taskWatch, gulp.series(taskSass));
