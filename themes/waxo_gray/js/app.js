24b4986e5be21aa853129f004cf46f8292f154e4
Jessica Wayde <jess@electriccitizen.com>
2026-06-25 11:42:13 -0700

    spruce up project pages a little

--- themes/waxo_gray/js/app.js
+++ themes/waxo_gray/js/app.js
@@ -9,10 +9,12 @@ jQuery('.field--field-images').slick();
 // Make project rows visible once they are past 1/3 the way up the screen.
 const project_rows = jQuery('.views-view--project-list .views-row');
 
-jQuery(window).on('resize scroll', () => {
-  project_rows.each((index, row) => {
-    if (jQuery(row).offset().top < (jQuery(window).scrollTop() + (jQuery(window).height() * 0.66))) {
-      jQuery(row).addClass('loaded');
-    }
-  })
-})
+if (project_rows.length > 0) {
+  jQuery(window).on('resize scroll', () => {
+    project_rows.each((index, row) => {
+      if (jQuery(row).offset().top < (jQuery(window).scrollTop() + (jQuery(window).height() * 0.66))) {
+        jQuery(row).addClass('loaded');
+      }
+    });
+  });
+}
