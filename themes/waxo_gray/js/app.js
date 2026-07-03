a9f353f81834e3e944862f9229f061e85d096c98
Jessica Wayde <jess@electriccitizen.com>
2026-07-03 11:19:41 -0700

    create toaster arrow animation to encourage scrolling

--- themes/waxo_gray/js/app.js
+++ themes/waxo_gray/js/app.js
@@ -6,16 +6,31 @@
 
 jQuery('.field--field-images').slick();
 
+
+// Make the toaster appear, and remove it when we've scrolled enough.
+const toaster = jQuery('.toaster');
+let toaster_min = getToasterMin();
+if (toaster.length > 0) {
+  toaster.addClass('visible');
+}
+
 // Make project rows visible once they are past 1/3 the way up the screen.
 const project_rows = jQuery('.views-view--project-list .views-row');
 project_rows.first().addClass('loaded');
 
 if (project_rows.length > 0) {
   jQuery(window).on('resize scroll', () => {
+    const distance = jQuery(window).scrollTop() + (jQuery(window).height() * 0.66);
+    toaster_min = getToasterMin();
+    toaster.toggleClass('finished', distance > toaster_min);
     project_rows.each((index, row) => {
-      if (jQuery(row).offset().top < (jQuery(window).scrollTop() + (jQuery(window).height() * 0.66))) {
+      if (jQuery(row).offset().top < distance) {
         jQuery(row).addClass('loaded');
       }
     });
   });
+}
+
+function getToasterMin() {
+  return jQuery(window).height() * 1;
 }
